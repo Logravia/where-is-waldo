3469523b6b38297e94b1e3e8f039eae7e41ae697
Jānis Alunāns <j.alunans@gmail.com>
2023-01-29 19:17:06 +0200

    Add fontawesome

--- src/components/Mark.js
+++ src/components/Mark.js
@@ -1,8 +1,13 @@
 import StyledMark from '../styles/Mark.styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCrosshairs } from '@fortawesome/free-solid-svg-icons'
+
 
 function Mark({showAt}) {
   return (
-    <StyledMark showAt={showAt}/>
+    <StyledMark showAt={showAt}>
+      <FontAwesomeIcon icon={faCrosshairs}/>
+    </StyledMark>
   )
 }
 
